c333f0b23bfb9897670e66f21c7e4394a08990ee
muzavan <muzavan@gmail.com>
2022-03-29 00:18:42 +0800

    reverse words

---
+++ top50strings/reverse_words_in_given_string.py
@@ -0,0 +1,26 @@
+# Problem: https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1/?page=1&curated[]=3&sortBy=submissions
+
+
+# User function Template for python3
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,S):
+        # code here 
+        originalWords = S.split(".")
+        
+        return ".".join(originalWords[::-1])
+
+#{ 
+#  Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        print(obj.reverseWords(s))
+
+# } Driver Code Ends
